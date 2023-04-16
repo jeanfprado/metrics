9680506c91241bbf92b31ac790ec1bc6b89df7b6
Jean Prado <jeanfprado@gmail.com>
2023-04-16 16:55:54 -0300

    defined config

---
+++ config/metrics.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Metrics Classes
+    |--------------------------------------------------------------------------
+    |
+    | Here you will define which metrics classes will be used, if you don't
+    | define a class here, will not be possible be used.
+    |
+    */
+    'classes' => [
+
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Route Middleware
+    |--------------------------------------------------------------------------
+    |
+    | These middleware will be assigned to every metrics route, giving you the
+    | chance to add your own middleware to this stack or override any of
+    | the existing middleware. Or, you can just stick with this stack.
+    |
+    */
+    'middleware' => [
+        'web'
+    ]
+];
